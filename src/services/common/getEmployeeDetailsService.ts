a76d663ea18a0f8ef2d7545860753fe4f38b4395
Syed Farhan <syedfarhan596@gmail.com>
2025-02-09 11:39:10 +0530

    fix profile

--- src/services/common/getEmployeeDetailsService.ts
+++ src/services/common/getEmployeeDetailsService.ts
@@ -1,43 +1,54 @@
-import path from 'path';
-import UserModel from '../../model/userModel';
+import path from "path";
+import UserModel from "../../model/userModel";
 
 interface getEmployeeDetailsResponse {
-    success: boolean;
-    employeeDetails?: any;
+  success: boolean;
+  employeeDetails?: any;
 }
 
-const getEmployeeDetails = (id: string): Promise<getEmployeeDetailsResponse> => {
-    return new Promise((resolve, reject) => {
-        UserModel.findOne({ _id: id })
-            .populate('bloodGroup')
-            .populate('employeeRole')
-            .populate('organization')
-            .then((employee: any) => {
-                if (!employee) {
-                    reject({ success: false });
-                } else {
-                    resolve({
-                        success: true,
-                        employeeDetails: {
-                            id: employee.id,
-                            firstName: employee.firstName,
-                            lastName: employee.lastName,
-                            email: employee.email,
-                            mobileNumber: employee.mobileNumber,
-                            bloodGroup: employee.bloodGroup,
-                            bankDetailsInfo: employee.bankDetailsInfo,
-                            employeeRole: employee.employeeRole,
-                            organization: employee.organization,
-                            profileImage: path.resolve(__dirname, '../../assets', 'profileImages', employee.profileImage)
-                        }
-                    });
-                }
-            })
-            .catch((error: any) => {
-                console.error('Error in getting employee details:', error);
-                reject({ success: false });
-            });
-    });
-}
+const getEmployeeDetails = (
+  id: string
+): Promise<getEmployeeDetailsResponse> => {
+  return new Promise((resolve, reject) => {
+    UserModel.findOne({ _id: id })
+      .populate("bloodGroup")
+      .populate("employeeRole")
+      .populate("organization")
+      .then((employee: any) => {
+        if (!employee) {
+          reject({ success: false });
+        } else {
+          resolve({
+            success: true,
+            employeeDetails: {
+              id: employee.id,
+              firstName: employee.firstName,
+              lastName: employee.lastName,
+              email: employee.email,
+              mobileNumber: employee.mobileNumber,
+              bloodGroup: employee.bloodGroup,
+              bankDetailsInfo: employee.bankDetailsInfo,
+              employeeRole: employee.employeeRole,
+              organization: employee.organization,
+              userRole: employee.userRole,
+              passwordResetRequired: employee.passwordResetRequired,
+              profileImage: employee.profileImage
+                ? path.resolve(
+                    __dirname,
+                    "../../assets",
+                    "profileImages",
+                    employee.profileImage
+                  )
+                : "",
+            },
+          });
+        }
+      })
+      .catch((error: any) => {
+        console.error("Error in getting employee details:", error);
+        reject({ success: false });
+      });
+  });
+};
 
 export default { getEmployeeDetails };
