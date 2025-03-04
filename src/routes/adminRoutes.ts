cb540f3977a096bccd2eae2c8e15d1f02c9c2cf5
YadagiriTalada <yadagiri_talada@epam.com>
2025-03-04 15:43:19 +0530

    Updated sytax issues

--- src/routes/adminRoutes.ts
+++ src/routes/adminRoutes.ts
@@ -26,99 +26,27 @@ import deletePoolCandidateByadminController from '../controllers/admin/deletePoo
 
 const adminRouter: Router = express.Router();
 
-adminRouter.post("/login", commonController.login);
-adminRouter.get("/refreshToken", commonController.refreshToken);
-adminRouter.get("/logout", validateJWT, commonController.logout);
-adminRouter.post(
-  "/registerEmployeeByAdmin",
-  validateJWT,
-  registerEmployeeByAdminController.register
-);
-adminRouter.get(
-  "/getEmployeeDetailsByAdmin/:id",
-  validateJWT,
-  getEmployeeDetailsByAdminController.getUserDetails
-);
-adminRouter.put(
-  "/updateEmployeeDetailsByAdmin",
-  validateProfileRequest(userSchema),
-  validateJWT,
-  updateEmployeeDetailsByAdminController.updateProfile
-);
-adminRouter.get(
-  "/getAllEmployeeDetailsByAdmin",
-  validateJWT,
-  getAllEmployeeDetailsByAdminController.getAllEmployeeDetails
-);
-adminRouter.post(
-  "/employeePasswordResetByAdmin",
-  employeePasswordResetByAdminController.employeePasswordResetByAdmin
-);
-adminRouter.get(
-  "/getAllBloodGroupsByAdmin",
-  validateJWT,
-  getAllBloodGroupsByAdminController.getAllBloodGroupsDetails
-);
-adminRouter.post(
-  "/addBloodGroupByAdmin",
-  validateJWT,
-  addBloodGroupByAdminController.addNewBloodgroupByAdmin
-);
-adminRouter.post(
-  "/deleteEmployeeByAdmin",
-  validateJWT,
-  deleteEmployeeDetailsByAdminController.deleteProfile
-);
-adminRouter.delete(
-  "/deleteBloodGroupByAdmin/:id",
-  validateJWT,
-  deleteBloodGroupByAdminController.deleteBloodGroup
-);
-adminRouter.put(
-  "/updateBloodGroupByAdmin",
-  validateJWT,
-  updateBloodGroupByAdminController.updateBloodGroup
-);
-adminRouter.post(
-  "/addEmploymentTypeByAdmin",
-  validateJWT,
-  addEmploymentTypeByAdminController.addEmploymentTypeByAdmin
-);
-adminRouter.get(
-  "/getallEmploymentTypesByAdmin",
-  validateJWT,
-  getAllEmploymentTypesByAdminController.getAllEmploymentTypesByAdmin
-);
-adminRouter.put(
-  "/updateEmploymentTypeByAdmin",
-  validateJWT,
-  updateEmploymentTypeByAdminController.updateEmploymentType
-);
-adminRouter.delete(
-  "/deleteEmploymentTypeByAdmin/:id",
-  validateJWT,
-  deleteEmploymentTypeByAdminController.deleteEmploymentType
-);
-adminRouter.post(
-  "/addEmployeeRoleByAdmin",
-  validateJWT,
-  addEmployeeRoleByAdminController.addEmployeeRoleByAdmin
-);
-adminRouter.get(
-  "/getAllEmployeeRoleByAdmin",
-  validateJWT,
-  getAllEmployeeRoleByAdminController.getAllEmployeeRolesByAdmin
-);
-adminRouter.put(
-  "/updateEmployeeRoleByAdmin",
-  validateJWT,
-  updateEmployeeRoleByAdminController.updateEmployeeRole
-);
-adminRouter.delete(
-  "/deleteEmployeeRoleByAdmin/:id",
-  validateJWT,
-  deleteEmployeeRoleByAdminController.deleteEmployeeRole
-);;
+adminRouter.post('/login', commonController.login);
+adminRouter.get('/refreshToken', commonController.refreshToken);
+adminRouter.get('/logout', validateJWT, commonController.logout);
+adminRouter.post('/registerEmployeeByAdmin', validateJWT, registerEmployeeByAdminController.register);
+adminRouter.get('/getEmployeeDetailsByAdmin/:id', validateJWT, getEmployeeDetailsByAdminController.getUserDetails);
+adminRouter.put('/updateEmployeeDetailsByAdmin', validateProfileRequest(userSchema), validateJWT, updateEmployeeDetailsByAdminController.updateProfile);
+adminRouter.get('/getAllEmployeeDetailsByAdmin', validateJWT, getAllEmployeeDetailsByAdminController.getAllEmployeeDetails);
+adminRouter.post('/employeePasswordResetByAdmin', employeePasswordResetByAdminController.employeePasswordResetByAdmin);
+adminRouter.get('/getAllBloodGroupsByAdmin', validateJWT, getAllBloodGroupsByAdminController.getAllBloodGroupsDetails);
+adminRouter.post('/addBloodGroupByAdmin', validateJWT, addBloodGroupByAdminController.addNewBloodgroupByAdmin);
+adminRouter.post('/deleteEmployeeByAdmin', validateJWT, deleteEmployeeDetailsByAdminController.deleteProfile);
+adminRouter.delete('/deleteBloodGroupByAdmin/:id', validateJWT, deleteBloodGroupByAdminController.deleteBloodGroup);
+adminRouter.put('/updateBloodGroupByAdmin', validateJWT, updateBloodGroupByAdminController.updateBloodGroup);
+adminRouter.post('/addEmploymentTypeByAdmin', validateJWT, addEmploymentTypeByAdminController.addEmploymentTypeByAdmin);
+adminRouter.get('/getallEmploymentTypesByAdmin', validateJWT, getAllEmploymentTypesByAdminController.getAllEmploymentTypesByAdmin);
+adminRouter.put('/updateEmploymentTypeByAdmin', validateJWT, updateEmploymentTypeByAdminController.updateEmploymentType);
+adminRouter.delete('/deleteEmploymentTypeByAdmin/:id', validateJWT, deleteEmploymentTypeByAdminController.deleteEmploymentType);
+adminRouter.post('/addEmployeeRoleByAdmin', validateJWT, addEmployeeRoleByAdminController.addEmployeeRoleByAdmin);
+adminRouter.get('/getAllEmployeeRoleByAdmin', validateJWT, getAllEmployeeRoleByAdminController.getAllEmployeeRolesByAdmin);
+adminRouter.put('/updateEmployeeRoleByAdmin', validateJWT, updateEmployeeRoleByAdminController.updateEmployeeRole);
+adminRouter.delete('/deleteEmployeeRoleByAdmin/:id', validateJWT, deleteEmployeeRoleByAdminController.deleteEmployeeRole);
 adminRouter.delete('/deletePoolCandidatesByAdmin/:id', validateJWT, deletePoolCandidateByadminController.deletePoolCandidateByAdmin);
 
 export default adminRouter;
