dad629afe4873c8a618146896ee007eb2a2f5b06
YadagiriTalada <yadagiri_talada@epam.com>
2025-03-13 12:38:54 +0530

    Update Pacakge API

--- src/interfaces/package.ts
+++ src/interfaces/package.ts
@@ -1,7 +1,6 @@
 import { Document } from 'mongoose';
 
 export interface IPackage extends Document {
-    id?: number;
     title?: string;
     description?: string;
     startDate?: Date;
--- src/services/admin/updatePackageByAdminService.ts
+++ src/services/admin/updatePackageByAdminService.ts
@@ -1,23 +1,22 @@
-import { any } from 'joi';
-import PackagesModel from '../../model/packageModel'
-import { IPackage } from '../../interfaces/package'
+import PackagesModel from '../../model/packageModel';
+import { IPackage } from '../../interfaces/package';
 
 interface updatePackageResponse {
     success: boolean;
-    responseAfterupdate?: any;
+    responseAfterUpdate?: any;
 }
 
 const updatePackageByAdmin = async (id: string, detailsToUpdate: IPackage): Promise<updatePackageResponse> => {
     try {
         const result = await PackagesModel.updateOne({ _id: id }, { ...detailsToUpdate });
         if (result) {
-            return { success: true, responseAfterupdate: result };
+            return { success: true, responseAfterUpdate: result };
         } else {
             return { success: false };
         }
     } catch (error: any) {
         console.error(`Error in updating packages: ${error}`)
-        return { success: false, responseAfterupdate: error }
+        return { success: false, responseAfterUpdate: error }
     }
 }
 
