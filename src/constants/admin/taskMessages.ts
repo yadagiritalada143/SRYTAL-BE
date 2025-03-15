1d877cb9d0c434a67f475e0fc13c1a12565b975e
unknown <Ramyathummala>
2025-03-15 09:36:58 +0530

    addTaskToPackageByAdmin

--- src/constants/admin/taskMessages.ts
+++ src/constants/admin/taskMessages.ts
@@ -1,6 +1,5 @@
 export const TASK_ERROR_MESSAGES = {
-    TASK_ADD_ERROR_MESSAGE: 'Error occured while adding the tASKS !!',
-   
+    TASK_ADD_ERROR_MESSAGE: 'Error occured while adding the tASKS !!', 
 };
 
 
