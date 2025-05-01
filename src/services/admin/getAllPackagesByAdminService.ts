08a49cea09b7bb4865fda731827d2830d13bce75
YadagiriTalada <yadagiri_talada@epam.com>
2025-05-01 13:13:17 +0530

    Fixed packages issue

--- src/services/admin/getAllPackagesByAdminService.ts
+++ src/services/admin/getAllPackagesByAdminService.ts
@@ -8,14 +8,6 @@ interface FetchPackagesDetailsResponse {
 const getAllPackagesByAdmin = (): Promise<FetchPackagesDetailsResponse> => {
     return new Promise((resolve, reject) => {
         PackagesModel.find({})
-            .populate({
-                path: 'tasks',
-                populate: {
-                    path: 'createdBy',
-                    select: 'firstName lastName',
-                },
-            })
-            .populate('approvers', 'firstName lastName')
             .then((pacakgesList: any) => {
                 if (!pacakgesList) {
                     reject({ success: false });
