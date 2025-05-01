f3bd0cc2fb34dc883eefae16a3e04a5279c71f97
YadagiriTalada <yadagiri_talada@epam.com>
2025-05-01 13:44:01 +0530

    Added fix for all the packages list along with tasks

--- src/services/admin/getPackageDetailsByAdminService.ts
+++ src/services/admin/getPackageDetailsByAdminService.ts
@@ -1,29 +1,34 @@
+import { AnyARecord } from 'node:dns';
 import PackagesModel from '../../model/packageModel';
+import TaskModel from '../../model/taskModel';
 
 interface FetchPackagesDetailsResponse {
     success: boolean;
     packageDetails?: any;
 }
 
-const getPackageDetailsByAdmin = (id: string): Promise<FetchPackagesDetailsResponse> => {
-    return new Promise((resolve, reject) => {
-        PackagesModel.findById({ _id: id })
-            .populate('approvers', 'firstName lastName')
-            .then((packageDetails: any) => {
-                if (!packageDetails) {
-                    reject({ success: false });
-                } else {
-                    resolve({
-                        success: true,
-                        packageDetails: packageDetails
-                    });
-                }
-            })
-            .catch((error: any) => {
-                console.error('Error in fetching Pacakge details:', error);
-                reject({ success: false });
-            });
-    });
+const getPackageDetailsByAdmin = async (id: string): Promise<FetchPackagesDetailsResponse> => {
+    try {
+        const packageDoc = await PackagesModel.findById(id)
+            .populate('approvers', 'firstName lastName');
+
+        if (!packageDoc) {
+            return { success: false };
+        }
+
+        const taskDetails = await TaskModel.find({ packageId: id });
+
+        const packageDetails = packageDoc.toObject() as any;
+        packageDetails.tasks = taskDetails;
+
+        return {
+            success: true,
+            packageDetails
+        };
+    } catch (error) {
+        console.error('Error in fetching Package details:', error);
+        return { success: false };
+    }
 };
 
-export default { getPackageDetailsByAdmin }
+export default { getPackageDetailsByAdmin };
