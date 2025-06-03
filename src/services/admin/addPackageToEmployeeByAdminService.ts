ed65b539212e2add8e11317ab210d2b36d0169e9
ramyathummala <ramyathummala10.com>
2025-06-03 12:34:13 +0530

    adding new feature

--- src/services/admin/addPackageToEmployeeByAdminService.ts
+++ src/services/admin/addPackageToEmployeeByAdminService.ts
@@ -2,9 +2,22 @@
 import EmployeePackageModel from '../../model/employeePackageModel';
 import { IEmployeePackage } from '../../interfaces/employeepackages';
 
+
 const addPackagetoEmployeeByAdmin = async (data: IEmployeePackage): Promise<any> => {
-    const packagetoEmployeTData = new EmployeePackageModel(data);
-    return await packagetoEmployeTData.save();
+    const {employeeId, packageId } = data;
+    const existingPackage = await EmployeePackageModel.findOne({ employeeId, packageId });
+
+    if(existingPackage) {
+        return await EmployeePackageModel.findOneAndUpdate(
+            {employeeId, packageId }, data,
+            {new: true}
+        );
+    }else{
+          const packagetoEmployeTData = new EmployeePackageModel(data);
+    return await packagetoEmployeTData.save();        
+
+    }
+  
 }
 
 export default { addPackagetoEmployeeByAdmin }
