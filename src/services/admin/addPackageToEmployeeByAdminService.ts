b39aa47a129bbd8284f83c2f3024604f1b7b0b6f
YadagiriTalada <yadagiri_talada@epam.com>
2025-06-04 20:29:50 +0530

    Refactored

--- src/services/admin/addPackageToEmployeeByAdminService.ts
+++ src/services/admin/addPackageToEmployeeByAdminService.ts
@@ -1,23 +1,20 @@
 
 import EmployeePackageModel from '../../model/employeePackageModel';
-import { IEmployeePackage } from '../../interfaces/employeepackages';
 
 
-const addPackagetoEmployeeByAdmin = async (data: IEmployeePackage): Promise<any> => {
-    const {employeeId, packageId } = data;
+const addPackagetoEmployeeByAdmin = async (data: any): Promise<any> => {
+    const { employeeId, packageId } = data;
     const existingPackage = await EmployeePackageModel.findOne({ employeeId, packageId });
 
-    if(existingPackage) {
+    if (existingPackage) {
         return await EmployeePackageModel.findOneAndUpdate(
-            {employeeId, packageId }, data,
-            {new: true}
+            { employeeId, packageId }, data,
+            { new: true }
         );
-    }else{
-          const packagetoEmployeTData = new EmployeePackageModel(data);
-    return await packagetoEmployeTData.save();        
-
+    } else {
+        const packagetoEmployeTData = new EmployeePackageModel(data);
+        return await packagetoEmployeTData.save();
     }
-  
 }
 
 export default { addPackagetoEmployeeByAdmin }
