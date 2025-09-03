a22deb0b9bead4318947a9e3ae33d1b22ca8990d
YadagiriTalada <yadagiri_talada@epam.com>
2025-09-04 01:07:25 +0530

    Refactored code

--- src/routes/contentwriterRoutes.ts
+++ src/routes/contentwriterRoutes.ts
@@ -1,18 +1,17 @@
 import express, { Router } from 'express';
 import validateJWT from '../middlewares/validateJWT';
+import addCourseController from '../controllers/contentwriter/addCourseController';
 import getAllCoursesController from '../controllers/contentwriter/getAllCoursesController';
 import getCourseByIdController from '../controllers/contentwriter/getCourseByIdController';
 import addCourseModuleController from '../controllers/contentwriter/addCourseModuleController';
 import addTaskController from '../controllers/contentwriter/addTaskController';
-import addCourseController from '../controllers/contentwriter/addCourseController';
-
 
 const contentwriterRouter: Router = express.Router();
 
+contentwriterRouter.post('/addCourse', validateJWT, addCourseController.addNewCourseController);
 contentwriterRouter.get('/getAllCourses', validateJWT, getAllCoursesController.getAllCourses);
 contentwriterRouter.get('/getCourseById/:id', validateJWT, getCourseByIdController.getCourseByIdController);
 contentwriterRouter.post('/addCourseModule', validateJWT, addCourseModuleController.addNewCourseModuleController);
 contentwriterRouter.post('/addCourseTask', validateJWT, addTaskController.addNewCourseTaskController);
-contentwriterRouter.post('/addCourse', validateJWT, addCourseController.addNewCourseController)
 
 export default contentwriterRouter;
