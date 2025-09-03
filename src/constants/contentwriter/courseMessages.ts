90a5d7c96d9d5663ff89c46993a39e5aac619b57
YadagiriTalada <yadagiri_talada@epam.com>
2025-09-04 00:39:16 +0530

    Added missed constant

--- src/constants/contentwriter/courseMessages.ts
+++ src/constants/contentwriter/courseMessages.ts
@@ -6,6 +6,6 @@ export const COURSE_ADD_SUCCESS_MESSAGES = {
 export const COURSE_ERRORS_MESSAGES = {
     COURSE_ADD_ERROR_MESSAGE: 'Error occured while adding course !',
     COURSE_ADD_FETCH_ERROR_MESSAGES: 'Error occured while fetching the courses !',
-    
+    COURSE_FETCH_ERROR_MESSAGES: 'Error in fetching course details !',
 };
 
