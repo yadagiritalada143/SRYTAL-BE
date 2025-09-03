456a403bb50764c7ee6644571cba10bcd32bfab7
YadagiriTalada <yadagiri_talada@epam.com>
2025-09-04 02:00:39 +0530

    Refactored code for all courses related APIs

--- src/constants/contentwriter/courseMessages.ts
+++ src/constants/contentwriter/courseMessages.ts
@@ -1,8 +1,8 @@
 export const COURSE_SUCCESS_MESSAGES = {
-    COURSE_ADD_SUCCESS_MESSAGE: 'New course added Successfully !',
+    COURSE_ADD_SUCCESS_MESSAGE: 'New Course added Successfully !',
 }
 
 export const COURSE_ERRORS_MESSAGES = {
-    COURSE_ADD_ERROR_MESSAGE: 'Error occured while adding course !',
+    COURSE_ADD_ERROR_MESSAGE: 'Error occured while adding new Course !',
     COURSE_FETCH_ERROR_MESSAGES: 'Error in fetching course details !',
 };
--- src/constants/contentwriter/coursetaskMessages.ts
+++ src/constants/contentwriter/coursetaskMessages.ts
@@ -1,7 +1,7 @@
 export const COURSES_TASK_ADD_SUCCESS_MESSAGES = {
-    COURSE_TASK_ADD_SUCCESS_MESSAGE: 'New course task added Successfully !',
+    COURSE_TASK_ADD_SUCCESS_MESSAGE: 'New Task added to Module Successfully !',
 }
 
 export const COURSE_TASK_ADD_ERRORS_MESSAGES = {
-    COURSE_TASK_ADD_ERROR_MESSAGE: 'Error occured while adding course task !',
+    COURSE_TASK_ADD_ERROR_MESSAGE: 'Error occured while adding Task to Module !',
 };
