45279480d95a55d60c4085b2d270e57fd35850f1
ramyathummala <ramyathummala10@gmail.com>
2025-09-03 15:12:53 +0530

    module api created

--- src/constants/contentwriter/coursemoduleMessages.ts
+++ src/constants/contentwriter/coursemoduleMessages.ts
@@ -1,8 +1,8 @@
 export const COURSESMODULE_ADD_SUCCESS_MESSAGES = {
-    COURSEMODULE_ADD_SUCCESS_MESSAGE: 'New coursemodule added Successfully !',
+    COURSEMODULE_ADD_SUCCESS_MESSAGE: 'New module added Successfully !',
 }
 
 export const COURSEMODULE_ADD_ERRORS_MESSAGES = {
-    COURSEMODULE_ADD_ERROR_MESSAGE: 'Error occured while adding coursemodule !',
+    COURSEMODULE_ADD_ERROR_MESSAGE: 'Error occured while adding module !',
     
 };
