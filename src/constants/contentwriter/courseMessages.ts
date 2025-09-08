d44d6194b1dc8a1350de9c1a86dc00a170ccdd5b
ramyathummala <ramyathummala10@gmail.com>
2025-09-08 11:40:41 +0530

    Api for update the course

--- src/constants/contentwriter/courseMessages.ts
+++ src/constants/contentwriter/courseMessages.ts
@@ -1,8 +1,11 @@
 export const COURSE_SUCCESS_MESSAGES = {
     COURSE_ADD_SUCCESS_MESSAGE: 'New Course added Successfully !',
-}
+};
 
 export const COURSE_ERROR_MESSAGES = {
     COURSE_ADD_ERROR_MESSAGE: 'Error occured while adding new Course !',
     COURSE_FETCH_ERROR_MESSAGE: 'Error in fetching course details !',
+    COURSE_UPDATE_ERROR_MESSAGE: 'Error occurred while updating the Course !',
+    COURSE_INVALID_STATUS_MESSAGE: 'Please provide valid status !',
+
 };
