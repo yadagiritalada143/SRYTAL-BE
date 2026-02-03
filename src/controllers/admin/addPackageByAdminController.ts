0ee1190d507d6faf2f86acf43eb62ee8dd16c605
ramyathummala <ramyathummala10@gmail.com>
2026-02-03 20:33:47 +0530

    swagger doc added for packages and code changes

--- src/controllers/admin/addPackageByAdminController.ts
+++ src/controllers/admin/addPackageByAdminController.ts
@@ -2,19 +2,6 @@ import { Request, Response } from "express";
 import addPackageByAdminService from "../../services/admin/addPackageByAdminService";
 import { PACKAGE_ERROR_MESSAGES, PACKAGE_SUCCESS_MESSAGES, HTTP_STATUS } from "../../constants/admin/packageMessages";
 
-// const addPackageByAdminController = (req: Request, res: Response) => {
-//     const addPackageDetails = req.body;
-//     addPackageDetails.isDeleted = false;
-//     addPackageByAdminService
-//         .addPackageByAdmin(addPackageDetails)
-//         .then((responseAfteraddingPackages: any) => {
-//             res.status(200).json({ succes: true });
-//         })
-//         .catch((error: any) => {
-//             console.error(`Error while adding packages: ${error}`);
-//             res.status(500).json({ success: false, message: PACKAGE_ERROR_MESSAGES.PACKAGE_ADD_ERROR_MESSAGE });
-//         });
-// };
 const addPackageByAdminController = async (req: Request, res: Response) => {
     try{
         const addPackageDetails = req.body;
