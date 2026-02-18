6fd1fcc59cd1490f45cfded684eb99f42e6fd60e
YadagiriTalada <yadagiritalada@gmail.com>
2026-02-18 21:56:12 +0530

    Added feedback attributes APIs

--- src/controllers/admin/deleteFeedbackAttributeByAdminController.ts
+++ src/controllers/admin/deleteFeedbackAttributeByAdminController.ts
@@ -1,5 +1,5 @@
-import {Request, Response} from 'express';
-import deleteFeedbackAttribute from '../../services/admin/deleteFeedbackeAttributeByAdminService';
+import { Request, Response } from 'express';
+import deleteFeedbackAttribute from '../../services/admin/deleteFeedbackAttributeByAdminService';
 import { FEEDBACK_ATTRIBUTE_ERROR_MESSAGES, FEEDBACK_ATTRIBUTE_SUCCESS_MESSAGES, HTTP_STATUS } from '../../constants/admin/feedbackattributeMessages';
 
 const deleteFeedbackAttributeByAdminController = async (req: Request, res: Response): Promise<Response> => {
@@ -7,7 +7,7 @@ const deleteFeedbackAttributeByAdminController = async (req: Request, res: Respo
         const id = req.params.id;
         const deleteResult = await deleteFeedbackAttribute.deleteFeedbackAttributeByAdminService(id);
 
-        if(deleteResult.success) {
+        if (deleteResult.success) {
             return res.status(HTTP_STATUS.OK).json({ success: true, message: FEEDBACK_ATTRIBUTE_SUCCESS_MESSAGES.FEEDBACK_ATTRIBUTE_DELETE_SUCCESS_MESSAGE, data: deleteResult });
         } else {
             return res.status(HTTP_STATUS.NOT_FOUND).json({ success: false, message: FEEDBACK_ATTRIBUTE_ERROR_MESSAGES.FEEDBACK_ATTRIBUTE_NOT_FOUND_ERROR_MESSAGE });
