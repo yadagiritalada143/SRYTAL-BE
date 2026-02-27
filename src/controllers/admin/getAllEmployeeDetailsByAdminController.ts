22319213f38ec2bff20c1a00e329950fb31ea106
ramyathummala <ramyathummala10@gmail.com>
2026-02-27 14:44:11 +0530

    changes

--- src/controllers/admin/getAllEmployeeDetailsByAdminController.ts
+++ src/controllers/admin/getAllEmployeeDetailsByAdminController.ts
@@ -4,8 +4,7 @@ import allEmployeeDetailsServices from '../../services/admin/getAllEmployeeDetai
 
 const getAllEmployeeDetails = async (req: Request, res: Response) => {
     try {
-        const { organizationId, userId } = req.body; // Assuming organizationId and userId are sent in the request body
-
+        const { organizationId, userId } = req.params; 
 
         const fetchAllEmployeeDetailsByAdminResponse =
             await allEmployeeDetailsServices.getAllEmployeeDetailsByAdmin(
