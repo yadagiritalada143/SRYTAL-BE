0ee8f2bd6c4ce5664411f65f71073a0a3841d412
ramyathummala <ramyathummala10@gmail.com>
2026-03-06 19:20:45 +0530

    Create API to Get Department Details by ID

--- src/controllers/admin/getDepartmentByAdminController.ts
+++ src/controllers/admin/getDepartmentByAdminController.ts
@@ -1,11 +1,11 @@
 import { Request, Response } from 'express';
-import getDepartmentDetails from '../../services/admin/getDepartmentByAdminService';
+import getDepartmentByAdminService from '../../services/admin/getDepartmentByAdminService';
 import { DEPARTMENT_ERROR_MESSAGES, DEPARTMENT_SUCCESS_MESSAGES, HTTP_STATUS } from '../../constants/admin/departmentMessages';
 
 const getDepartmentByAdmin = async (req: Request, res: Response): Promise<Response> => {
     try {
         const { _id } = req.params;
-        const departmentDetails = await getDepartmentDetails.getDepartmentByAdmin(_id);  
+        const departmentDetails = await getDepartmentByAdminService.getDepartmentByAdmin(_id);  
         if (!departmentDetails) {
             return res.status(HTTP_STATUS.NOT_FOUND).json({ success: false, message: DEPARTMENT_ERROR_MESSAGES.DEPARTMENT_NOT_FOUND_ERROR_MESSAGE });
         }
