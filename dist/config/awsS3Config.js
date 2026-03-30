fe776908680b14f5bd3e0cde3e79e4bd916e2f0e
YadagiriTalada <yadagiritalada@gmail.com>
2026-03-30 14:53:42 +0530

    Cleaned up dist

--- dist/config/awsS3Config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.salarySlipsFolder = exports.courseModuleThumbnailsFolder = exports.coursesThumbnailsFolder = exports.profileImagesFolder = exports.bucketName = void 0;
-exports.bucketName = 'srytal-documents';
-exports.profileImagesFolder = 'ProfileImages';
-exports.coursesThumbnailsFolder = 'LMSData/Courses/CourseThumbnails';
-exports.courseModuleThumbnailsFolder = 'LMSData/Courses/CourseModuleThumbnail';
-exports.salarySlipsFolder = 'SalarySlips';
--- dist/types/validCourseStatusTypes.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.validStatusValues = void 0;
-exports.validStatusValues = ['ARCHIVE', 'ACTIVE'];
-exports.default = exports.validStatusValues;
